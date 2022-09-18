a8c01a5ab270c4792f29e6c93f4a8d20792a8bb7
Emiiinm <112517128+Emiiinm@users.noreply.github.com>
2022-09-18 10:57:44 -0600

    Delete Unidad#1 semana#1 clase#2 ejercicio #1.cpp

--- Unidad#1 semana#1 clase#2 ejercicio #1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-	double nota;
-	cout << "Ingresar la nota del examen de informatica" <<endl;
-	cin >> nota;
-	cout << "La nota del examen es: "  << nota << endl;
-}
